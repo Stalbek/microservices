7aece888d35d241dccc5a5190a43c9ce6b6a3d08
StalbekR <stalbekr@gmail.com>
2021-12-09 02:14:57 +0600

    repo

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'KZT', 440, '');
+
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'KZT', 500, '');
+
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'RUB', 'KZT', 6, '');
+
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10004, 'KGS', 'KZT', 5, '');
